aa2973a08f05fe995495ce35ac2f94c28947d7e2
Zygar <a@andywilkinson.co.nz>
2017-12-06 10:04:18 +1300

    Allows a user to update the rendered data by twiddling some knobs and dials

--- src/App.vue
+++ src/App.vue
@@ -14,33 +14,26 @@ import sourceData from './_data.js';
 import InfoView from './components/InfoView.vue';
 import NavigationView from './components/NavigationView.vue';
 
-function selectCurrent(req) {
-    if(req != undefined) {
-        console.log("returning", req)
-        console.log(sourceData.children[req])
-        return sourceData.children[req];    
-    }
-    else return sourceData;
-    // return sourceData;
-    // return sourceData.children[0];
-    // return sourceData.children[0].children[0].children[0];
-}
-
 export default {
     name: 'app', 
     components: { InfoView, NavigationView },
     data () {
         let current = {
-            selected: selectCurrent(),
-            callNext: ""
+            callNext: "",
+            allItems: sourceData,
+            selected: {}
         }
         return current;
     }, 
     methods: {
         updateInfo: function(e) {
-            console.log(this.callNext);
-            selectCurrent(this.callNext);
-            
+            this.selectCurrent(this.callNext);
+        },
+        selectCurrent: function(req) {
+            console.log(req);
+            this.selected = this.allItems.children[0].children[req];
+            // console.log(this.selected)
+            // return this.allItems.children[0]
         }
     }
 }
