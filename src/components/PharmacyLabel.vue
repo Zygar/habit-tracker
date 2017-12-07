04f2ffecf53901e2ecdd56094e361248b4c47ff5
Zygar <a@andywilkinson.co.nz>
2017-12-08 10:37:18 +1300

    makes pharmacy label example conditional

--- src/components/PharmacyLabel.vue
+++ src/components/PharmacyLabel.vue
@@ -1,7 +1,7 @@
 <template v-if="label">
     <div id="PharmacyLabel" class="component">
         <div class="label-description" v-html="label.labelDescription"></div>
-        <div class="label-example">
+        <div class="label-example" v-if="label.labelVerb">
         <p>{{label.labelVerb}} <span class="label-meta  labelDose">{{label.labelDose}}</span> <span class="label-meta  labelFrequency">{{label.labelFrequency}}</span>.</p>
         </div>
     </div>
